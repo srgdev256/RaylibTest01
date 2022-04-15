bcf8878f33bcd5b98a871b322c7093fcdf1a5132
srgdev256 <srgdev256@gmail.com>
2022-04-15 19:57:19 +0300

    initial project setup

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace RaylibTest01
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Raylib.InitWindow(1280, 800, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
