9f0b26f443f4fd25b0862b0548d7baec3c34d3c3
srgdev256 <srgdev256@gmail.com>
2022-04-15 20:30:48 +0300

    blue ball added

---
+++ BlueBall.cs
@@ -0,0 +1,50 @@
+﻿using Raylib_cs;
+
+namespace RaylibTest01
+{
+    public class BlueBall
+    {
+        private int _x;
+        private int _y;
+
+        private int _speed;
+        
+        public int X => _x;
+        public int Y => _y;
+        
+        public BlueBall(int x, int y, int speed)
+        {
+            _x = x;
+            _y = y;
+            _speed = speed;
+        }
+
+        public void Update()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                _x -= _speed;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                _x += _speed;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+            {
+                _y -= _speed;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+            {
+                _y += _speed;
+            }
+        }
+
+        public void Draw()
+        {
+            Raylib.DrawCircle(_x, _y, 48.0f, Color.BLUE);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,68 @@ namespace RaylibTest01
 {
     static class Program
     {
+        public const int Width = 1280;
+        public const int Height = 800;
+        
         public static void Main()
         {
-            Raylib.InitWindow(1280, 800, "Hello World");
+            //INIT
+            Raylib.SetConfigFlags(ConfigFlags.FLAG_VSYNC_HINT);
+            Raylib.InitWindow(Width, Height, "Hello World");
 
+            
+            int shift = 0;
+            int redBallShift;
+
+            BlueBall blueBall = new BlueBall(500, 500, 3);
+            
+            //GAME LOOP
             while (!Raylib.WindowShouldClose())
             {
+                //1. update
+                
+                //red ball update
+                shift += 1;
+
+                if (shift == 200)
+                {
+                    shift = 0;
+                }
+
+                if (shift < 100)
+                {
+                    redBallShift = shift;
+                }
+                else
+                {
+                    redBallShift = 100 - (shift - 100);
+                }
+
+                //blue ball update
+                blueBall.Update();
+                
+                
+                //2. draw
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
 
                 Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
 
+                //line
+                Raylib.DrawLine(200 + redBallShift, 100, blueBall.X, blueBall.Y, Color.GRAY);
+                
+                //blue ball draw
+                blueBall.Draw();
+                
+                //red ball draw
+                Raylib.DrawCircle(200 + redBallShift, 100, 32.0f, Color.RED);
+                
+
+                
                 Raylib.EndDrawing();
             }
 
+            //CLEANUP
             Raylib.CloseWindow();
         }
     }
